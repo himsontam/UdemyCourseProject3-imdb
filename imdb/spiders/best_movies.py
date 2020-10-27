00396efdf4424f8fb80c7628636594bfd7ad3fb4
Himson <himsom0528@gmail.com>
2020-10-26 23:42:35 -0500

    Update best_movies.py

--- imdb/spiders/best_movies.py
+++ imdb/spiders/best_movies.py
@@ -31,5 +31,6 @@ class BestMoviesSpider(CrawlSpider):
             'duration': response.xpath("normalize-space((//time)[1]/text())").get(),
             'genre': response.xpath("//div[@class='subtext']/a[1]/text()").get(),
             'rating': response.xpath("//span[@itemprop='ratingValue']/text()").get(),
-            'movie_url': response.url
+            'movie_url': response.url,
+            'user-agent': response.request.header['User-agent']
         }
